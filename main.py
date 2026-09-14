6602764a78e356904cd73eb3a41cb01cd048e52e
bitOfAPhilistine <philipgowell@gmail.com>
2026-09-14 01:15:01 -0400

    Refactored state system to be cleaner, any states that need to preserve data will now have their own loops

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from internal.transform2D import Transform2D
 from internal.timer import Timer
 from pythonosc import udp_client
 import tkinter as tk
-import config, openvr, math, time
+import config, openvr, math, time, asyncio
 
 
 running = True
 state: function = None
-initState = False
 ovr: openvr.IVRSystem = None
 
 hipTrackerIndex = None
@@ -43,34 +42,69 @@ alert = canvas.create_text(
     fill="white", anchor="center", font=("Ubuntu", 40), justify="center", width=config.CANVAS_WIDTH / 2
 )
 
-class Indicator:
-    def __init__(self, transform: Transform2D, scale: float, canvasBounds: tuple[Vector2, Vector2]):
-        self.transform = transform
-        self.scale = scale
-        self.canvasBounds = canvasBounds
+@profiler
+def getDeltaTimeAndWait(exit: bool = False):
+    dt = 0
+    lastCalled = time.time()
 
-def on_close():
+    yield config.TARGET_FRAMERATE
+
+    while not exit:
+        dt = time.time() - lastCalled
+        if dt < config.TARGET_FRAMERATE:
+            time.sleep(config.TARGET_FRAMERATE - dt)
+            yield config.TARGET_FRAMERATE
+        else:
+            yield dt
+
+    return dt
+
+def setState(newState: function) {
+    global state
+    state = newState
+    print(f"State set to {state.__qualname__}")
+}
+
+def close():
+    print("Exiting...")
     global running
     running = False
     root.destroy()
-root.protocol("WM_DELETE_WINDOW", on_close)
-
-def setState(newState: function):
-    global state, initState
-    state = newState
-    initState = True
-    print(f"State set to {state.__qualname__ if state != None else None}")
-
-def setStateButton(newState: function) -> function:
-    def inner():
-        global state, initState
-        state = newState
-        initState = True
-        print(f"State set to {state.__qualname__}")
-    return inner
+root.protocol("WM_DELETE_WINDOW", close)
 
 @profiler
-def findHipTracker(dt: float):
+def initialize():
+    if ovr == None:
+        try:
+            ovr = openvr.init(openvr.VRApplication_Background)
+            print("OpenVR initialized")
+        except openvr.error_code.InitError_Init_NoServerForBackgroundApp:
+            canvas.itemconfig(alert, text="SteamVR not found")
+            if state != None:
+                setState(None)
+            ovr = None
+    elif hipTrackerIndex == None:
+        try:
+            with open("hip-tracker-serial-number.txt", "r") as f:
+                hipTrackerSerial = f.read()
+                for i in range(openvr.k_unMaxTrackedDeviceCount):
+                    if ovr.getTrackedDeviceClass(i) == openvr.TrackedDeviceClass_GenericTracker and hipTrackerSerial == ovr.getStringTrackedDeviceProperty(i, openvr.Prop_SerialNumber_String):
+                        hipTrackerIndex = i
+                        print(f"Hip tracker identified at index {i}, serial {hipTrackerSerial}")
+                        break
+                if hipTrackerIndex == None:
+                    canvas.itemconfig(alert, text="Enable your hip tracker, or identify it if it is already on")
+        except:
+            print("hip-tracker-serial-number.txt not found or invalid")
+            setState(findHipTracker)
+            hipTrackerIndex = -1
+    elif offset == None:
+        canvas.itemconfig(alert, text="Hip offset not set, please calibrate")
+    else:
+        setState(None)
+
+@profiler
+def findHipTracker():
     tracker = None
     for i in range(openvr.k_unMaxTrackedDeviceCount):
         if ovr.getTrackedDeviceClass(i) == openvr.TrackedDeviceClass_GenericTracker:
@@ -90,81 +124,82 @@ def findHipTracker(dt: float):
     print(f"Hip tracker identified as {hipTrackerSerial}")
     with open("hip-tracker-serial-number.txt", "w") as f:
         f.write(hipTrackerSerial)
+    
     setState(None)
 
 findHipTrackerButton = tk.Button(
     root,
     text="Set hip tracker",
-    command=setStateButton(findHipTracker)
+    command=findHipTracker
 )
 findHipTrackerButton.place(anchor="sw", relx=0, rely=1, relheight=1/8, relwidth=1/3)
 
-lastOffset: Transform2D
-avgOffset: Transform2D
-calibrateOffsetTimer: Timer
-
 @profiler
-def calibrateOffset(dt: float):
-    global initState, offset, lastOffset, avgOffset, calibrateOffsetTimer
+def calibrateOffset():
+    global offset
 
-    headTransform = getDeviceTransform(0)
-    hipTransform = getDeviceTransform(hipTrackerIndex)
+    lastOffset: Transform2D
+    avgOffset: Transform2D
+    calibrateOffsetTimer = Timer(1)
 
-    if headTransform == None:
-        print("Error: head transform is none!")
-        return
-    elif hipTransform == None:
-        print("Error: hip transform is none!")
-        return
+    while running:
+        dt = getDeltaTimeAndWait()
 
-    currentOffset = Transform2D(hipTransform.position - headTransform.position, hipTransform.rotation - headTransform.rotation)
+        headTransform = getDeviceTransform(0)
+        hipTransform = getDeviceTransform(hipTrackerIndex)
 
-    if initState:
-        lastOffset = currentOffset.copy()
-        avgOffset = currentOffset.copy()
-        calibrateOffsetTimer = Timer(1)
+        if headTransform == None:
+            print("Error: head transform is none!")
+            break
+        elif hipTransform == None:
+            print("Error: hip transform is none!")
+            break
 
-        initState = False
+        currentOffset = Transform2D(hipTransform.position - headTransform.position, hipTransform.rotation - headTransform.rotation)
 
-    if lastOffset.sq_distance(currentOffset) >= config.DIST_THRES ** 2:
-        canvas.itemconfig(alert, text="Please hold still")
-        calibrateOffsetTimer.restart()
-        avgOffset = currentOffset.copy()
-        print("Too much movement, calibration restarted")
-    elif calibrateOffsetTimer.done():
-        offset = avgOffset
-        setState(None)
-        print(f"Calibration complete, set to {offset}")
-    else:
-        canvas.itemconfig(alert, text="Calibrating...")
-        progress = calibrateOffsetTimer.progress()
-        avgOffset.position = avgOffset.position * progress + currentOffset.position * (1 - progress)
-        avgOffset.rotation = avgOffset.rotation * progress + currentOffset.rotation * (1 - progress)
-        print(f"Current Offset: {currentOffset}")
-        print(f"Average Offset: {avgOffset}")
+        if lastOffset and lastOffset.sq_distance(currentOffset) >= config.DIST_THRES ** 2:
+            canvas.itemconfig(alert, text="Please hold still")
+            calibrateOffsetTimer.restart()
+            avgOffset = currentOffset.copy()
+            print("Too much movement, calibration restarted")
+        elif calibrateOffsetTimer.done():
+            offset = avgOffset
+            print(f"Calibration complete, set to {offset}")
+            break
+        else:
+            canvas.itemconfig(alert, text="Calibrating...")
+            progress = calibrateOffsetTimer.progress()
+            avgOffset.position = avgOffset.position * progress + currentOffset.position * (1 - progress)
+            avgOffset.rotation = avgOffset.rotation * progress + currentOffset.rotation * (1 - progress)
+            print(f"Current Offset: {currentOffset}")
+            print(f"Average Offset: {avgOffset}")
 
-    lastOffset = currentOffset
+        lastOffset = currentOffset
+        root.update()
+
+    setState(None)
+    getDeltaTimeAndWait(exit=True)
 
 calibrateOffsetButton = tk.Button(
     root,
     text="Calibrate hip offset",
-    command=setStateButton(calibrateOffset)
+    command=calibrateOffset
 )
 calibrateOffsetButton.place(anchor="s", relx=0.5, rely=1, relheight=1/8, relwidth=1/3)
 
 @profiler
-def activeNoPath(dt: float):
+def activeNoPath():
     pass
 
 activateButton = tk.Button(
     root,
     text="Activate",
-    command=setStateButton(activeNoPath)
+    command=activeNoPath
 )
 activateButton.place(anchor="se", relx=1, rely=1, relheight=1/8, relwidth=1/3)
 
 @profiler
-def activeWithPath(dt: float):
+def activeWithPath():
     pass
 
 frame.pack()
@@ -172,54 +207,15 @@ canvas.pack()
 
 
 if __name__ == "__main__":
-    dt = config.TARGET_FRAMERATE
+    setState(initialize)
 
     while running:
         canvas.itemconfig(alert, text="")
 
-        if ovr == None:
-            try:
-                ovr = openvr.init(openvr.VRApplication_Background)
-                print("OpenVR initialized")
-            except openvr.error_code.InitError_Init_NoServerForBackgroundApp:
-                canvas.itemconfig(alert, text="SteamVR not found")
-                if state != None:
-                    setState(None)
-                ovr = None
-        elif hipTrackerIndex == None:
-            try:
-                with open("hip-tracker-serial-number.txt", "r") as f:
-                    hipTrackerSerial = f.read()
-                    for i in range(openvr.k_unMaxTrackedDeviceCount):
-                        if ovr.getTrackedDeviceClass(i) == openvr.TrackedDeviceClass_GenericTracker and hipTrackerSerial == ovr.getStringTrackedDeviceProperty(i, openvr.Prop_SerialNumber_String):
-                            hipTrackerIndex = i
-                            print(f"Hip tracker identified at index {i}, serial {hipTrackerSerial}")
-                            break
-                    if hipTrackerIndex == None:
-                        canvas.itemconfig(alert, text="Enable your hip tracker, or identify it if it is already on")
-            except:
-                print("hip-tracker-serial-number.txt not found or invalid")
-                setState(findHipTracker)
-                hipTrackerIndex = -1
-        elif offset == None:
-            canvas.itemconfig(alert, text="Hip offset not set, please calibrate")
-
-        startTime = time.time()
-
         if state != None:
-            state(dt)
+            state()
 
         if offset != None:
-            print(offset)
+            print(f"Offset: {offset}")
 
-        try:
-            root.update()
-        except tk.TclError:
-            break
-
-        frameTime = time.time() - startTime
-        if frameTime < config.TARGET_FRAMERATE:
-            dt = config.TARGET_FRAMERATE
-            time.sleep(config.TARGET_FRAMERATE - frameTime)
-        else:
-            dt = frameTime
+        root.update()
